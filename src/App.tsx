58401fb8ba60be7463599c1f8dbb6a6d515078ce
holydexboi <holydexboi@yahoo.com>
2023-05-04 10:46:35 +0100

    add dividend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import ExpandableText from "./ExpandableText";
+import Form from "./Form";
 function App() {
 
-  return <div><ExpandableText maxChar={10}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti unde eveniet quo voluptatibus omnis alias, enim voluptatem necessitatibus cumque id aut iste reprehenderit beatae quisquam tenetur officia. Tenetur, autem dignissimos.</ExpandableText></div>;
+  return <div><Form/></div>;
 }
 export default App;
